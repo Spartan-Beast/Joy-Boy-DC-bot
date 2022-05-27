bb502b9a6d7334e2bbcbe3f069bb5334000081ec
Spartan-Beast <77671045+Spartan-Beast@users.noreply.github.com>
2022-05-27 15:48:01 +0400

    Update commands.py

--- cogs/commands.py
+++ cogs/commands.py
@@ -27,7 +27,7 @@ class Commands(commands.Cog):
         embed = discord.Embed(title='Help', description=f'Here is the list of commands for this bot:\nUse {configData["Prefix"]}help <cmd> for extended information on that command.', color=ctx.author.color)
         embed.add_field(name='Joy Boy commands', value='joke, opjoke, bmjoke, meme, ameme, tmeme, bmeme', inline=False)
         embed.add_field(name='Fights', value='straw hats, luffy, zoro, sanji, nami, robin, usopp, sogeking', inline=False)
-        embed.add_field(name='Miscellaneous', value='clear', inline=False)
+        embed.add_field(name='Mod commands', value='announce, clear', inline=False)
         embed.set_footer(icon_url=ctx.author.avatar_url, text=f"Requested by {ctx.author.name}")
         await ctx.send(embed=embed)
             
@@ -39,6 +39,13 @@ class Commands(commands.Cog):
         embed.set_footer(icon_url=ctx.author.avatar_url, text=f"Requested by {ctx.author.name}")
         await ctx.send(embed=embed)
 
+    @help.command()
+    async def announce(self, ctx):
+        embed = discord.Embed(title='Announce', description='Running this command in a bot-cmd channel will send an announcement message to the server\'s announcements channel. Write your message after the command.', color=ctx.author.color)
+        embed.add_field(name='**Syntax**', value=f'{configData["Prefix"]}announce <message>')
+        embed.set_footer(icon_url=ctx.author.avatar_url, text=f"Requested by {ctx.author.name}")
+        await ctx.send(embed=embed)
+        
     @help.command()
     async def bmjoke(self, ctx):
         embed = discord.Embed(title='Big Mom joke', description='Sends a random Big Mom ~~joke~~ fact.', color=ctx.author.color)
